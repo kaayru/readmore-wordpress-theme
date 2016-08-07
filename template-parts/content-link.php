f5be7c4f0194eca17d5b727847e3772ef1b6f410
Vincent Ballut <kaayru@gmail.com>
2016-08-07 14:29:42 +0200

    Adds style for link post format

---
+++ template-parts/content-link.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ReadMore
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php if( false && has_post_thumbnail() ) : ?>
+		<div class="hentry-thumbnail">
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+				<img src="<?php the_post_thumbnail_url() ?>" alt="<?php the_title(); ?>">
+			</a>
+		</div>
+	<?php endif; ?>
+
+	<header class="entry-header">
+		<?php
+		if ( is_single() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		endif;
+
+		if ( is_single() && 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php readmore_posted_on(); ?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
+
+		<div class="post-format-icon">
+		</div>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			the_content();
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'readmore' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<?php if ( is_single() ) : ?>
+		<footer class="entry-footer">
+			<?php readmore_entry_footer(); ?>
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
+</article><!-- #post-## -->
