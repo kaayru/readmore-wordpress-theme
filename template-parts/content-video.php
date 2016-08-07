132869888aa37fd64e447573ff5dbaf49f367124
Vincent Ballut <kaayru@gmail.com>
2016-08-07 14:48:28 +0200

    Styles video post format

---
+++ template-parts/content-video.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ReadMore
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<div class="hentry-thumbnail video-container">
+		<?php echo readmore_get_first_embed(get_the_ID()); ?>
+	</div>
+
+	<header class="entry-header">
+		<?php
+		if ( is_single() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" title="' . get_the_title() . '" rel="bookmark">', '</a></h2>' );
+		endif;
+
+		if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php readmore_posted_on(); ?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
+
+		<div class="post-format-icon">
+		</div>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			if ( is_single() ) :
+				the_content( sprintf(
+					/* translators: %s: Name of current post. */
+					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'readmore' ), array( 'span' => array( 'class' => array() ) ) ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				) );
+
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'readmore' ),
+					'after'  => '</div>',
+				) );
+			else :
+				the_excerpt();
+			endif;
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php readmore_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
