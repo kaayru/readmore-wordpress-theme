e0a81cee0249573affeac318084a51c4855d9a7a
Vincent Ballut <kaayru@gmail.com>
2016-08-15 17:45:47 +0200

    Moves customizer.js on inc

--- inc/customizer.php
+++ inc/customizer.php
@@ -28,7 +28,7 @@ if ( ! function_exists( 'readmore_customize_preview_js' ) ) :
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function readmore_customize_preview_js() {
-	wp_enqueue_script( 'readmore_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'readmore_customizer', get_template_directory_uri() . '/inc/customizer/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 endif;
 add_action( 'customize_preview_init', 'readmore_customize_preview_js' );
